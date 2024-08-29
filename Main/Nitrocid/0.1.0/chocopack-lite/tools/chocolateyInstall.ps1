d28bcc0764235d67cb6e1d2c5da796d6c4cf8c45
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-08-29 17:47:55 +0300

    upd - Updated Chocolatey for 0.1.0.19
    ---
    
    Type: upd
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Main/Nitrocid/0.1.0/chocopack-lite/tools/chocolateyInstall.ps1
+++ Main/Nitrocid/0.1.0/chocopack-lite/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "kslite"
-$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0.18/0.1.0.18-bin-lite.zip"
-$shacheck   = "2f2360f5930c9fafb17ac2939d8a89bbbc47dc6b1d31ea45ef172bdaf927cec4"
+$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0.19/0.1.0.19-bin-lite.zip"
+$shacheck   = "06cc82a843c1f3c7669a3f5e859a4d638db8f7f098c99ef03532a286e85788da"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
--- Main/Nitrocid/0.1.0/chocopack/tools/chocolateyInstall.ps1
+++ Main/Nitrocid/0.1.0/chocopack/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "KS"
-$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0.18/0.1.0.18-bin.zip"
-$shacheck   = "1d38b192ed2bd25d3c8b8708448f9916300e0fc6deb659aba0121fac41460e9f"
+$url        = "https://github.com/Aptivi/NitrocidKS/releases/download/v0.1.0.19/0.1.0.19-bin.zip"
+$shacheck   = "7f13bfef70e754d567be36b66ef1ea904478bb75b8d6ba83cf8e38ebf97f32f6"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
