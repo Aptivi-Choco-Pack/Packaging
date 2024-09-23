ec12c46fb3f31dd9511ced66007ee568d11e8492
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-09-23 23:02:57 +0300

    fix - Fixed pkgName in BassBoom pack
    ---
    
    KS's packaging doesn't have to do with BB's packaging.
    
    ---
    
    Type: fix
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Main/BassBoom/0.2.x/chocopack/tools/chocolateyInstall.ps1
+++ Main/BassBoom/0.2.x/chocopack/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pkgName    = "KS"
+$pkgName    = "bassboom"
 $url        = "https://github.com/Aptivi/BassBoom/releases/download/v0.2.1/0.2.1-cli.zip"
 $shacheck   = "ab6710e249d15db863898112aeb9f5b4aaa76c30cff25a5ded195f9801cbc206"
 
