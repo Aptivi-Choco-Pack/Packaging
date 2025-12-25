e6acfe7bab1af8f53aa853f211a39d8bd0963d1f
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-25 21:22:26 +0300

    pkg - Added Nitrocid 0.2.0 RC
    ---
    
    Type: pkg
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Main/Nitrocid/0.2.0/chocopack-lite/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pkgName    = "kslite"
+$url        = "https://github.com/Aptivi/Nitrocid/releases/download/v0.2.0-rc/0.2.0.0-rc-bin-lite.zip"
+$shacheck   = "E96D4C666C9329A19495C01FAEA0EAC8D364AC45DB1CC93C99819DE4B416B94A"
+
+Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+Write-Output "<*> Installation directory: $toolsDir"
+Write-Output "<*> Package Name: $pkgName"
+Write-Output "<*> URL: $url"
+Write-Output "<*> Expected SHA256 Sum: $shacheck"
+Write-Output "<+> Configuration will be automatically generated on startup."
+
+Install-ChocolateyZipPackage $pkgName $url $toolsDir -ChecksumType "sha256" -Checksum $shacheck
---
+++ Main/Nitrocid/0.2.0/chocopack/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pkgName    = "KS"
+$url        = "https://github.com/Aptivi/Nitrocid/releases/download/v0.2.0-rc/0.2.0.0-rc-bin.zip"
+$shacheck   = "D2A62448070C0C35393FD59765D695B9C586B561A18BE8B5BBDDE5B36BF88CD8"
+
+Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+Write-Output "<*> Installation directory: $toolsDir"
+Write-Output "<*> Package Name: $pkgName"
+Write-Output "<*> URL: $url"
+Write-Output "<*> Expected SHA256 Sum: $shacheck"
+Write-Output "<+> Configuration will be automatically generated on startup."
+
+Install-ChocolateyZipPackage $pkgName $url $toolsDir -ChecksumType "sha256" -Checksum $shacheck
