14258677045f9a1f245ab177f0147b8b0173bae8
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-02-15 13:43:46 +0300

    pkg - Updated BassBoom packaging to 0.2.13.3
    ---
    
    Type: pkg
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- Main/BassBoom/0.2.x/chocopack/tools/chocolateyInstall.ps1
+++ Main/BassBoom/0.2.x/chocopack/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "bassboom"
-$url        = "https://github.com/Aptivi/BassBoom/releases/download/v0.2.13.2/0.2.13.2-cli.zip"
-$shacheck   = "787D87DC07A50387E928D54D448462C28217389A4811D483CD119544F6138A2D"
+$url        = "https://github.com/Aptivi/BassBoom/releases/download/v0.2.13.3/0.2.13.3-cli.zip"
+$shacheck   = "2477F5418902943ECC5A45B7DDF8BCB66BAEA415FE2F62E6788E1E1DAFA29953"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
