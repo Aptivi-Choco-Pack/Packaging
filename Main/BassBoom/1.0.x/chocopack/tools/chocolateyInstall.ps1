5513033a0a55750cd2c2268fdc98190420aae058
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-02-17 14:41:15 +0300

    add - Added BassBoom 1.0 package
    ---
    
    This is done in preparation for the development of this version of
    BassBoom.
    
    ---
    
    Type: add
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

---
+++ Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pkgName    = "bassboom"
+$url        = "https://github.com/Aptivi/BassBoom/releases/download/v1.0.0/1.0.0.0-cli.zip"
+$shacheck   = "2477F5418902943ECC5A45B7DDF8BCB66BAEA415FE2F62E6788E1E1DAFA29953"
+
+Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
+Write-Output "<*> Installation directory: $toolsDir"
+Write-Output "<*> Package Name: $pkgName"
+Write-Output "<*> URL: $url"
+Write-Output "<*> Expected SHA256 Sum: $shacheck"
+
+Install-ChocolateyZipPackage $pkgName $url $toolsDir -ChecksumType "sha256" -Checksum $shacheck
