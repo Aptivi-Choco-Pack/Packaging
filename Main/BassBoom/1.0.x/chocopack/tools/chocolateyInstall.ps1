5e72812efd0f940b97ce33a44490e738e0360e33
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-05-02 22:02:07 +0300

    pkg - Updated Chocolatey for BB 1.0
    ---
    
    Type: pkg
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
+++ Main/BassBoom/1.0.x/chocopack/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pkgName    = "bassboom"
-$url        = "https://github.com/Aptivi/BassBoom/releases/download/v1.0.0/1.0.0.0-cli.zip"
-$shacheck   = "2477F5418902943ECC5A45B7DDF8BCB66BAEA415FE2F62E6788E1E1DAFA29953"
+$url        = "https://github.com/Aptivi/BassBoom/releases/download/v1.0.0/1.0.0-bin.zip"
+$shacheck   = "DAFFA4D562B606C660369628BFC162893057967A6864C4459BEBC0B2704E2309"
 
 Write-Output "<*>: for assumptions, <+> for progress, <-> for error"
 Write-Output "<*> Installation directory: $toolsDir"
